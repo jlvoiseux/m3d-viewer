5bde0344a009eadbe261423469277fda64931dbf
Jean-Louis Voiseux <jlouis.vx@gmail.com>
2022-11-10 00:26:09 +0000

    Base project

--- Versus/pch.h
+++ Versus/pch.h
@@ -5,31 +5,6 @@
 
 #pragma once
 
-// DirectXTK12 headers
-#include "BufferHelpers.h"
-#include "CommonStates.h"
-#include "DDSTextureLoader.h"
-#include "DescriptorHeap.h"
-#include "DirectXHelpers.h"
-#include "EffectPipelineStateDescription.h"
-#include "Effects.h"
-#include "GamePad.h"
-#include "GeometricPrimitive.h"
-#include "GraphicsMemory.h"
-#include "Keyboard.h"
-#include "Model.h"
-#include "Mouse.h"
-#include "PostProcess.h"
-#include "PrimitiveBatch.h"
-#include "RenderTargetState.h"
-#include "ResourceUploadBatch.h"
-#include "ScreenGrab.h"
-#include "SimpleMath.h"
-#include "SpriteBatch.h"
-#include "SpriteFont.h"
-#include "VertexTypes.h"
-#include "WICTextureLoader.h"
-
 #include <winsdkver.h>
 #ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0A00
@@ -91,6 +66,31 @@
 #include <stdexcept>
 #include <system_error>
 #include <tuple>
+
+// DirectXTK12 headers
+#include "BufferHelpers.h"
+#include "CommonStates.h"
+#include "DDSTextureLoader.h"
+#include "DescriptorHeap.h"
+#include "DirectXHelpers.h"
+#include "EffectPipelineStateDescription.h"
+#include "Effects.h"
+#include "GamePad.h"
+#include "GeometricPrimitive.h"
+#include "GraphicsMemory.h"
+#include "Keyboard.h"
+#include "Model.h"
+#include "Mouse.h"
+#include "PostProcess.h"
+#include "PrimitiveBatch.h"
+#include "RenderTargetState.h"
+#include "ResourceUploadBatch.h"
+#include "ScreenGrab.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+#include "WICTextureLoader.h"
 
 #ifdef _DEBUG
 #include <dxgidebug.h>
